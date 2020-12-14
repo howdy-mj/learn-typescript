a82b78ef987961612f8a940582e9150e1eed1ba2
kmj <hi.minjungkim@gmail.com>
2020-12-14 22:58:37 +0900

    타입 추론

--- example/9_type-inference.ts
+++ example/9_type-inference.ts
@@ -1,3 +1,4 @@
+// 타입 추론 기본 1
 var a = 'a';
 
 function logA(a = 'a') {
@@ -5,22 +6,32 @@ function logA(a = 'a') {
   return b;
 }
 
+// 타입 추론 기본 2
 interface Dropdown<T> {
-  value: T
+  value: T;
   title: string;
 }
 var items: Dropdown<number> = {
   value: 10,
-  title: 'a'
-}
+  title: 'a',
+};
+var shoppingItem: Dropdown<string> = {
+  value: 'shoe',
+  title: 'hoho',
+};
 
+// 타입 추론 기본 3
 interface DetailedDropdown<T> extends Dropdown<T> {
   description: string;
   tag: T;
 }
-var detailItems: DetailedDropdown<number> = {
+var detailItems: DetailedDropdown<string> = {
+  // T가 올라감
   value: 'hi',
   title: 'a',
   description: 'b',
-  tag: 'c'
-}
+  tag: 'c',
+};
+
+// Best common type
+var arr9 = [1, 2, true]; // 알아서 추론
